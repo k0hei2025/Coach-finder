2cd4a8d4313f6854d4d72c924eb0c1bc80f178f4
k0hei2025 <vaibhavvermaonline@gmail.com>
2022-01-13 16:30:02 +0530

    work in progress

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -1,36 +1,39 @@
 <template>
           <div class="parent-container">
                <h1>Register as a coach now!</h1>
+        <form v-on:submit.prevent="submitHandler()">
                <p>Firstname</p>
-<input type='text' v-model="formData.firstName"/>
+        
+                 <input type='text' v-model="formData.firstName"/>
 
-<p>Lastname</p>
-<input type='text' v-bind:model="formData.lastName"/>
+                 <p>Lastname</p>
+                 <input type='text' v-model="formData.lastName"/>
 
-<p>Description</p>
-<textarea   rows="5" v-bind:model="formData.description"/>
+                 <p>Description</p>
+                 <textarea   rows="5" v-model="formData.description"/>
 
-<p>Hourly Rate</p>
-<input type='number' v-bind:model="formData.HourlyRate" />
-
+                 <p>Hourly Rate</p>
+                 <input type='number' v-model="formData.HourlyRate" />
+                  <button type="submit">Submit</button>
+        </form>
 <p>Area of Expertise</p>
 
 <div class="form-control"> 
 
 <div style="display:flex">
-<input type="checkbox" id="frontend" value="frontend" v-model="frontendValue"/>
+<input type="checkbox" id="frontend" value="frontend" v-model="this.formData.area.frontend"/>
 <label for="frontend">frontend</label>
 </div>
 <div style="display:flex">
-<input type="checkbox" id="backend" value="backend" v-model="checkHandler" />
+<input type="checkbox" id="backend" value="backend" v-model="this.formData.area.backend" />
 <label for="backend">backend</label>
 </div>
 <div style="display:flex">
-<input type="checkbox" id="MachineLearning" value="MachineLearning"  v-model="checkHandler"/>
+<input type="checkbox" id="MachineLearning" value="MachineLearning"  v-model="this.formData.area.MachineLearning"/>
 <label for="MachineLearning">Machine Learning</label>
 </div>
 <div style="display:flex">
-<input type="checkbox" id="DataScience" value="DataScience" v-model="checkHandler" />
+<input type="checkbox" id="DataScience" value="DataScience" v-model="this.formData.area.DataScience" />
 <label for="DataScience">Data Science</label>
 </div>
 <base-button mode="inline">Register</base-button>
@@ -47,19 +50,45 @@ export default {
        
         data(){
                 return {
+                         areaAdd:[],
                         formData:{
                                 firstName: '',
                                 lastName: '',
                                 description: '',
                                 HourlyRate: 0,
-                                area: []
+                                area: {
+                                frontend:false,
+                                backend:false,
+                                MachineLearning:false,
+                                DataScience:false,
+                                }
                         },
-                        frontendValue:false
+                   
                 }
         },
-         watch(){
-             console.log(this.formData.firstName)
-        },
+        methods:{
+              
+
+         submitHandler(){
+             if(this.formData.area.frontend){
+                     this.areaAdd.push(this.formData.area.frontend)
+             }
+        
+             if(this.formData.area.backend){
+                     this.areaAdd.push(this.formData.area.backend)
+             }
+
+             if(this.formData.area.MachineLearning){
+                     this.areaAdd.push(this.formData.area.MachineLearning)
+             }
+
+             if(this.formData.area.DataScience){
+                     this.areaAdd.push(this.formData.area.DataScience)
+             }
+
+          console.log('values',this.formData , this.areaAdd)
+         }
+        }
 
         
               
